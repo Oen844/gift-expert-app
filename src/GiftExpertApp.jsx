73bc1be297257c7651ec6a88ed243adbd2a70964
Pau Egea <papau84@gmail.com>
2022-07-21 12:58:09 +0200

    state

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 
-const apiKey = 'C7IgKP3nG1CMgzOUV3OoYLQmE84SL8sQ';
+const apiKey = "C7IgKP3nG1CMgzOUV3OoYLQmE84SL8sQ";
 export const GiftExpertApp = () => {
-   return( 
-   <>
-        {/* titulo */}
+    const [categories, setCategories] = useState([" One Punch ", "Dragon ball"]);
+    console.log(categories);
+    
+    return (
+        <>
+            {/* titulo */}
 
-        <h1>GiftExpertApp</h1>
+            <h1>GiftExpertApp</h1>
 
-        {/* input */}
+            {/* input */}
 
-        {/* listado fe gif */}
-        {/* gif item */}
-    </>
-   )
-  
-}
+            {/* listado fe gif */}
+            <ol>
+                { categories.map( categoriy =>{
+                    return <li key={ categoriy }> { categoriy } </li>
+                }) }
+                <li>132</li>
+            </ol>
+            {/* gif item */}
+        </>
+    );
+};
