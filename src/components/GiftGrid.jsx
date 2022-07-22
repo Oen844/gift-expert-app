c4c4d82637968bced71682ce66f1fb23766e6026
Pau Egea <papau84@gmail.com>
2022-07-22 13:23:32 +0200

    useEffect

--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -1,14 +1,23 @@
 import { getGifts } from "../helpers/getGifs.js";
-
+import { useState , useEffect} from "react";
 
 export const GiftGrid = ({ category, key }) => {
 
-    getGifts(category);
+    const [counter, setCounter] = useState(10)
+    
+    useEffect( () => {
+        getGifts();
+    }, [  ])
+
+    
     
 
     return (
         <>
             <h3 key={key}>{category}</h3>
+
+            <h5>{ counter }</h5>
+            <button onClick={ ( ) => setCounter ( counter + 1 )}>+1</button>
 
         </>
     )
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifts = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=C7IgKP3nG1CMgzOUV3OoYLQmE84SL8sQ&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=C7IgKP3nG1CMgzOUV3OoYLQmE84SL8sQ&q=${category}&limit=10`;
 
     const resp = await fetch(url);
     const { data } = await resp.json();
